3e0ec4787ae93b1c3818b35679fb003b33f52717
Kaluchii <mr.kaluchii@mail.ru>
2017-03-07 18:53:08 +0600

    First commit

---
+++ app/Http/routes.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::auth();
+Route::group(['middleware' => 'auth', 'prefix' => 'adm'],function(){
+
+    Route::get('/',         'AdminController@getIndex');
+    // Таксаномия проекта. Визуальная зависимость данных.
+    Route::get('/taxonomy', 'Back\TaxonomyController@showTaxonomy');
+
+    // Служебные роуты
+    Route::post('/save', 'Back\SaveController@save');
+
+    // Создание нового элемента группы
+    Route::post('/newItemRow', 'Back\GroupItemController@newRow');
+    Route::post('/newItemBox', 'Back\GroupItemController@newBox');
+    Route::post('/newImage', 'Back\GroupItemController@newImageItem');
+
+    // Роут удаление элемента группы
+    Route::post('/removeItem', 'Back\GroupItemController@removeItem');
+});
+
