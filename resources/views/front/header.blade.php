3e36dad61dfa2b805cd3f899e38abf65d8fc0ac4
Kaluchii <mr.kaluchii@mail.ru>
2017-04-10 22:09:37 +0600

    Change phone numbers

--- resources/views/front/header.blade.php
+++ resources/views/front/header.blade.php
@@ -5,8 +5,8 @@
                 <img src="/img/logo.png" alt="Автоматизация общепита и торговли" class="header__logo-img">
             </div>
             <div class="header__communications">
-                <span class="header__phone">+7 (727) 220-20-00</span>
-                <span class="header__phone">+7 (747) 220-20-00</span>
+                <span class="header__phone">+7-906-272-26-91</span>
+                <span class="header__phone">+7-906-272-26-91</span>
                 <a href="mailto:spb@idsoft.info" class="header__mailto">spb@idsoft.info</a>
             </div>
             <div class="header__become-partner">
@@ -18,8 +18,8 @@
         <div class="sticky-header__wrap-1180">
             <p class="sticky-header__tagline">IDSOFT — автоматизация торговли и общепита</p>
             <div class="sticky-header__communications">
-                <span class="sticky-header__phone">+7 (727) 220-20-00</span>
-                <span class="sticky-header__phone">+7 (747) 220-20-00</span>
+                <span class="sticky-header__phone">+7-906-272-26-91</span>
+                <span class="sticky-header__phone">+7-906-272-26-91</span>
                 <a href="mailto:spb@idsoft.info" class="sticky-header__mailto">sale@idsoft.kz</a>
             </div>
             <div class="sticky-header__become-partner">
