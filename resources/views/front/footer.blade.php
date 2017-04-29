a2e177696e60ee994efd2d93c5068cc649b45632
Kaluchii <mr.kaluchii@mail.ru>
2017-04-29 23:31:30 +0600

    captcha test

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -59,7 +59,7 @@
 
                     <div class="partner-form__row partner-form__row--button">
                         <span class="partner-form__row-name">
-                            <input type="submit" value="Отправить" class="g-recaptcha partner-form__send-form"
+                            <input type="submit" value="Отправить" class="g-recaptcha partner-form__send-form send-form"
                                    id="recaptcha" data-size="invisible" data-sitekey="6Leo9B4UAAAAAG4fjAUWHYaHudbc6SZIDHlTVFso"
                                    data-callback="sendForm">
                         </span>
