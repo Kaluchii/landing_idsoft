47af5d18a201c9c5ad0c963273cf626903bc11f2
Kaluchii <mr.kaluchii@mail.ru>
2017-04-30 00:15:56 +0600

    captcha test

--- public/js/form.js
+++ public/js/form.js
@@ -206,23 +206,31 @@ $(document).ready(function(){
 
                 cResponse = {};
                 cResponse['g-recaptcha-response'] = grecaptcha.getResponse();
-                var captchaTest = $.ajax(
+                /*var captchaTest = */$.ajax(
                     {
                         type: 'POST',
                         url: '/captcha',
                         dataType: 'json',
-                        data: cResponse
+                        data: cResponse,
+                        success: function (response) {
+                            captchaResult = !response.error;
+                            console.log('Captcha success');
+                        },
+                        error: function (response) {
+                            captchaResult = false;
+                            console.log('Captcha error');
+                        }
                     }
                 );
 
-                captchaTest.success(function(data){
+                /*captchaTest.success(function(data){
                     captchaResult = !data.error;
                     console.log('Captcha success');
                 });
                 captchaTest.error(function(data){
                     captchaResult = false;
                     console.log('Captcha error');
-                });
+                });*/
             }
 
 
